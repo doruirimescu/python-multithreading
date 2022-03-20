e8c6f01a274535373b0802aa50b1d13adf8c9b8c
Doru Irimescu <doru.irimescu@sensible4.fi>
2022-03-20 22:19:21 +0200

    Add setup files

--- python_multithreading/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-setup(
-    name='python_multithreading',
-    version='0.1',
-    packages=['python_multithreading'],
-    url='https://github.com/doruirimescu/python-multithreading'
-)
